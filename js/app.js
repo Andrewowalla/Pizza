6887e3c502653eae7941415b41995381a0130d98
Andrewowalla <andrewowalla@gmail.com>
2021-09-21 09:27:47 +0300

    add user interface logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Business logic
 var price , crust_price, topping_price;
 let total = 0;
 
@@ -8,3 +9,17 @@ function getPizza (name, size, crust, topping, total) {
     this.topping =topping;
     this.total = total;
 }
+
+//User interface logic
+$(document).ready(function(){
+    $("button.order-button").click(function(event){
+    let pname = $(".flavor option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
+    $.each($("input[name='topping']:checked"), function(){            
+        ptopping.push($(this).val());
+    });
+    console.log(ptopping.join(", "));
+    });
+});
