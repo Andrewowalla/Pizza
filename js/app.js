151c9a70ed7829c075e889f801f0cc627e3e41c1
Andrewowalla <andrewowalla@gmail.com>
2021-09-23 12:17:03 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,23 +1,52 @@
-// Business logic
-function givePizza (flavor, size, crust, topping, total) {
-    this.flavor = flavor;
-    this.size = size;
-    this.crust= crust;
-    this.topping =topping;
-    this.total = total;
+//Businmess logic
+function getPizza (flavor, size, crust, topping, quantity, totalAmount) {
+  this.flavor = flavor;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.quantity = quantity;
+  this.totalAmount = totalAmount;
+  
 }
 //User interface logic
-$("button.order").click(function(event){
-    let flavor = $(".flavor option:selected").val();
-    let size = $("#size option:selected").val();
-    let crust = $("#crust option:selected").val();
-    let topping = [];
-    console.log(size)
-    let order = (flavor, size, crust, topping, total) => {
-        return {flavor, size, crust, topping, total};
+function getFlavor() {
+    var flavor = document.getElementById("flavor").value;
+    return parseInt(flavor);
+  }
+function getSize() {
+    var size = document.getElementById("size").value;
+    return parseInt(size);
+  }
+  
+  function getCrust() {
+    var crust = document.getElementById("crust").value;
+    return parseInt(crust);
+  }
+  
+  function getTopping() {
+    var topping = document.getElementById("topping").value;
+    return parseInt(topping);
+  }
+  function getQuantity() {
+    var number = document.getElementById("quantity").value;
+    return parseInt(number);
+  }
 
-    $.each($("input[name='toppings']:checked"), function(){            
-        topping.push($(this).val());
-   });
-}); 
-
+  function totalAmount() {
+    var totals = (getFlavor() + getSize() + getCrust() + getTopping())* getQuantity();
+    alert(
+      "You have Ordered " +
+      getQuantity("") +
+      " pizza," + 
+      "" +
+      " amounting to ksh. " +
+      totals +
+      "" +
+      " Thanks for your order welcome again."
+    );
+  
+    var location = prompt("If you want your pizza to be delivered to you please enter your location and if not cancel.");
+    if (location !== "") {
+      alert("You order will be delivered shortly.Delivery fee is 150/= and incase you didnt want it delivered please come for your order.");
+    }
+  }
